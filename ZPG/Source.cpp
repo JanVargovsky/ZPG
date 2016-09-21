c1488904012d1dc30cbc4eeab6534d68641cb8ae
Jan Vargovský <jan.vargovsky@gmail.com>
2016-09-21 07:35:31 +0000

    [School] Working triangle with modern opengl Imported from Changeset 14

--- ZPG/Source.cpp
+++ ZPG/Source.cpp
@@ -1,3 +1,6 @@
+//Include GLEW
+#include <GL/glew.h>
+
 //Include GLFW  
 #include <GLFW/glfw3.h>  
 
@@ -18,7 +21,23 @@ using namespace std;
 const int WIDTH = 640;
 const int HEIGHT = 480;
 
-GLfloat angle = 0;
+float points[] = {
+	0.0f, 0.5f, 0.0f,
+	0.5f, -0.5f, 0.0f,
+	-0.5f, -0.5f, 0.0f
+};
+const char* vertex_shader =
+"#version 330\n"
+"layout(location = 0) in vec3 vp;"
+"void main() {"
+" gl_Position = vec4(vp, 1.0);"
+"}";
+const char* fragment_shader =
+"#version 330\n"
+"out vec4 frag_colour;"
+"void main() {"
+" frag_colour = vec4(0.5, 0.0, 0.5, 1.0);"
+"}";
 
 static void error_callback(int error, const char* description) { fputs(description, stderr); }
 
@@ -34,17 +53,10 @@ static void window_focus_callback(GLFWwindow* window, int focused) { printf("win
 static void window_iconify_callback(GLFWwindow* window, int iconified) { printf("window_iconify_callback \n"); }
 
 static void window_size_callback(GLFWwindow* window, int width, int height) {
-	printf("resize %d, %d \n", width, height);
 	glViewport(0, 0, width, height);
 }
 
-static void cursor_callback(GLFWwindow *window, double x, double y)
-{
-	printf("cursor_callback \n");
-
-	const float borderOffset = WIDTH * 0.01f;
-	angle = ((float)((x - borderOffset) / (WIDTH - 2 * borderOffset))) * 360;
-}
+static void cursor_callback(GLFWwindow *window, double x, double y) { printf("cursor_callback \n"); }
 
 static void button_callback(GLFWwindow* window, int button, int action, int mode) {
 	if (action == GLFW_PRESS) printf("button_callback [%d,%d,%d]\n", button, action, mode);
@@ -71,10 +83,17 @@ int main(void)
 {
 	GLFWwindow* window;
 	glfwSetErrorCallback(error_callback);
-
-	if (!glfwInit())
+	if (!glfwInit()) {
+		fprintf(stderr, "ERROR: could not start GLFW3\n");
 		exit(EXIT_FAILURE);
-	window = glfwCreateWindow(WIDTH, HEIGHT, "ZPG", NULL, NULL);
+	}
+	/*
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	//*/
+	window = glfwCreateWindow(800, 600, "ZPG", NULL, NULL);
 	if (!window)
 	{
 		glfwTerminate();
@@ -82,79 +101,75 @@ int main(void)
 	}
 	glfwMakeContextCurrent(window);
 	glfwSwapInterval(1);
-
-	// Sets the key callback
-	glfwSetKeyCallback(window, key_callback);
-
-	glfwSetCursorPosCallback(window, cursor_callback);
-
-	glfwSetMouseButtonCallback(window, button_callback);
-
-	glfwSetWindowFocusCallback(window, window_focus_callback);
-
-	glfwSetWindowIconifyCallback(window, window_iconify_callback);
-
-	glfwSetWindowSizeCallback(window, window_size_callback);
-
-
+	// start GLEW extension handler
+	glewExperimental = GL_TRUE;
+	glewInit();
+	// get version info
+	printf("OpenGL Version: %s\n", glGetString(GL_VERSION));
+	printf("Using GLEW %s\n", glewGetString(GLEW_VERSION));
+	printf("Vendor %s\n", glGetString(GL_VENDOR));
+	printf("Renderer %s\n", glGetString(GL_RENDERER));
+	printf("GLSL %s\n", glGetString(GL_SHADING_LANGUAGE_VERSION));
+	int major, minor, revision;
+	glfwGetVersion(&major, &minor, &revision);
+	printf("Using GLFW %i.%i.%i\n", major, minor, revision);
 	int width, height;
 	glfwGetFramebufferSize(window, &width, &height);
 	float ratio = width / (float)height;
 	glViewport(0, 0, width, height);
 
+	//vertex buffer object (VBO)
+	GLuint VBO = 0;
+	glGenBuffers(1, &VBO); // generate the VBO
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(points), points, GL_STATIC_DRAW);
+	//vertex attribute object(VAO)
+	GLuint VAO = 0;
+	glGenVertexArrays(1, &VAO); //generate the VAO
+	glBindVertexArray(VAO); //bind the VAO
+	glEnableVertexAttribArray(0); //enable vertex attributes
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
 
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	glOrtho(-ratio, ratio, -1.f, 1.f, 1.f, -1.f);
+	//create and compile shaders
+	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertex_shader, NULL);
+	glCompileShader(vertexShader);
+	GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragment_shader, NULL);
+	glCompileShader(fragmentShader);
+	GLuint shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, fragmentShader);
+	glAttachShader(shaderProgram, vertexShader);
+	glLinkProgram(shaderProgram);
+	// Check shaders
+	GLint status;
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &status);
+	if (status == GL_FALSE)
+	{
+		GLint infoLogLength;
+		glGetProgramiv(shaderProgram, GL_INFO_LOG_LENGTH, &infoLogLength);
+		GLchar *strInfoLog = new GLchar[infoLogLength + 1];
+		glGetProgramInfoLog(shaderProgram, infoLogLength, NULL, strInfoLog);
+		fprintf(stderr, "Linker failure : %s\n", strInfoLog);
+		delete[] strInfoLog;
+	}
 
 
 	while (!glfwWindowShouldClose(window))
 	{
-		glClear(GL_COLOR_BUFFER_BIT);
-
-		glMatrixMode(GL_MODELVIEW);
-		glLoadIdentity();
-		glRotatef(angle, 0.f, 0.f, 1.f);
-		//glRotatef((float)glfwGetTime() * 1000.f, 0.f, 0.f, 1.f);
-
-		drawSquare();
-
-		glfwSwapBuffers(window);
-
+		// clear color and depth buffer
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		glUseProgram(shaderProgram);
+		glBindVertexArray(VAO);
+		// draw triangles
+		glDrawArrays(GL_TRIANGLES, 0, 3); //mode,first,count
+		// update other events like input handling
 		glfwPollEvents();
+		// put the stuff we’ve been drawing onto the display
+		glfwSwapBuffers(window);
 	}
 	glfwDestroyWindow(window);
 	glfwTerminate();
 	exit(EXIT_SUCCESS);
-}
-
-void drawTriangle()
-{
-	glBegin(GL_TRIANGLES);
-	glColor3f(1.f, 0.f, 0.f);
-	glVertex3f(-0.6f, -0.4f, 0.f);
-	glColor3f(0.f, 1.f, 0.f);
-	glVertex3f(0.6f, -0.4f, 0.f);
-	glColor3f(0.f, 0.f, 1.f);
-	glVertex3f(0.f, 0.6f, 0.f);
-	glEnd();
-}
-
-void drawSquare()
-{
-	glBegin(GL_POLYGON);
-
-	glColor3f(1.f, 0.f, 0.f);
-	glVertex3f(-0.5f, -0.5f, 0.f);
-
-	glColor3f(0.f, 1.f, 0.f);
-	glVertex3f(0.5f, -0.5f, 0.f);
-
-	glColor3f(0.f, 0.f, 1.f);
-	glVertex3f(0.5f, 0.5f, 0.f);
-
-	glColor3f(1.f, 1.f, 0.f);
-	glVertex3f(-0.5f, 0.5f, 0.f);
-
-	glEnd();
 }
