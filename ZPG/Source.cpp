9c885ef83e495c3f54a9e06ba6f36e9ef77c7023
Jan Vargovský <jan.vargovsky@gmail.com>
2016-09-21 08:33:43 +0000

    [School] Triangle + square (not working) Imported from Changeset 15

--- ZPG/Source.cpp
+++ ZPG/Source.cpp
@@ -15,69 +15,49 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <iostream>
+#include "Source.h"
 
 using namespace std;
 
 const int WIDTH = 640;
 const int HEIGHT = 480;
 
+//float points[] = {
+//	0.0f, 0.5f, 0.0f,
+//	0.5f, -0.5f, 0.0f,
+//	-0.5f, -0.5f, 0.0f
+//};
+
 float points[] = {
-	0.0f, 0.5f, 0.0f,
-	0.5f, -0.5f, 0.0f,
-	-0.5f, -0.5f, 0.0f
+	// Triangle points
+	0.0f, 0.5f, 0.0f, // bottom left
+	0.5f, -0.5f, 0.0f, // bottom right
+	-0.5f, -0.5f, 0.0f,  // top left
+
+	// square points
+	-0.5f, -0.5f, 0.0f, // bottom left
+	0.5f, -0.5f, 0.0f, // bottom right
+	-0.5f, 0.5f, 0.0f, // top left
+	0.5f, 0.5f, 0.0f, // top right
 };
+
 const char* vertex_shader =
 "#version 330\n"
 "layout(location = 0) in vec3 vp;"
+"out vec4 color;"
 "void main() {"
 " gl_Position = vec4(vp, 1.0);"
+" color = vec4(vp, 1.0);"
 "}";
 const char* fragment_shader =
 "#version 330\n"
+"in vec4 color;"
 "out vec4 frag_colour;"
 "void main() {"
-" frag_colour = vec4(0.5, 0.0, 0.5, 1.0);"
+" frag_colour = color;"
 "}";
 
 static void error_callback(int error, const char* description) { fputs(description, stderr); }
-
-static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
-{
-	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
-		glfwSetWindowShouldClose(window, GL_TRUE);
-	printf("key_callback [%d,%d,%d,%d] \n", key, scancode, action, mods);
-}
-
-static void window_focus_callback(GLFWwindow* window, int focused) { printf("window_focus_callback \n"); }
-
-static void window_iconify_callback(GLFWwindow* window, int iconified) { printf("window_iconify_callback \n"); }
-
-static void window_size_callback(GLFWwindow* window, int width, int height) {
-	glViewport(0, 0, width, height);
-}
-
-static void cursor_callback(GLFWwindow *window, double x, double y) { printf("cursor_callback \n"); }
-
-static void button_callback(GLFWwindow* window, int button, int action, int mode) {
-	if (action == GLFW_PRESS) printf("button_callback [%d,%d,%d]\n", button, action, mode);
-}
-
-//GLM test
-
-// Projection matrix : 45° Field of View, 4:3 ratio, display range : 0.1 unit <-> 100 units
-glm::mat4 Projection = glm::perspective(45.0f, 4.0f / 3.0f, 0.01f, 100.0f);
-
-// Camera matrix
-glm::mat4 View = glm::lookAt(
-	glm::vec3(10, 10, 10), // Camera is at (4,3,-3), in World Space
-	glm::vec3(0, 0, 0), // and looks at the origin
-	glm::vec3(0, 1, 0)  // Head is up (set to 0,-1,0 to look upside-down)
-);
-// Model matrix : an identity matrix (model will be at the origin)
-glm::mat4 Model = glm::mat4(1.0f);
-
-void drawTriangle();
-void drawSquare();
 
 int main(void)
 {
@@ -87,12 +67,7 @@ int main(void)
 		fprintf(stderr, "ERROR: could not start GLFW3\n");
 		exit(EXIT_FAILURE);
 	}
-	/*
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-	//*/
+
 	window = glfwCreateWindow(800, 600, "ZPG", NULL, NULL);
 	if (!window)
 	{
@@ -105,14 +80,8 @@ int main(void)
 	glewExperimental = GL_TRUE;
 	glewInit();
 	// get version info
-	printf("OpenGL Version: %s\n", glGetString(GL_VERSION));
-	printf("Using GLEW %s\n", glewGetString(GLEW_VERSION));
-	printf("Vendor %s\n", glGetString(GL_VENDOR));
-	printf("Renderer %s\n", glGetString(GL_RENDERER));
-	printf("GLSL %s\n", glGetString(GL_SHADING_LANGUAGE_VERSION));
-	int major, minor, revision;
-	glfwGetVersion(&major, &minor, &revision);
-	printf("Using GLFW %i.%i.%i\n", major, minor, revision);
+	PrintVersions();
+
 	int width, height;
 	glfwGetFramebufferSize(window, &width, &height);
 	float ratio = width / (float)height;
@@ -124,25 +93,62 @@ int main(void)
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(points), points, GL_STATIC_DRAW);
 	//vertex attribute object(VAO)
-	GLuint VAO = 0;
-	glGenVertexArrays(1, &VAO); //generate the VAO
-	glBindVertexArray(VAO); //bind the VAO
+	GLuint VAO_Triangle = 0;
+	glGenVertexArrays(1, &VAO_Triangle); //generate the VAO
+	glBindVertexArray(VAO_Triangle); //bind the VAO
 	glEnableVertexAttribArray(0); //enable vertex attributes
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
 
-	//create and compile shaders
+	GLuint VAO_Square = 0;
+	glGenVertexArrays(1, &VAO_Square); //generate the VAO
+	glBindVertexArray(VAO_Square); //bind the VAO
+	glEnableVertexAttribArray(0); //enable vertex attributes
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+
+	// Create & compile vertex shader
 	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
 	glShaderSource(vertexShader, 1, &vertex_shader, NULL);
 	glCompileShader(vertexShader);
+	// Create & compile fragment shader
 	GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
 	glShaderSource(fragmentShader, 1, &fragment_shader, NULL);
 	glCompileShader(fragmentShader);
+	// Create shader program that consist of fragment and vertex shaders
 	GLuint shaderProgram = glCreateProgram();
-	glAttachShader(shaderProgram, fragmentShader);
 	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
 	glLinkProgram(shaderProgram);
 	// Check shaders
+	CheckShaders(shaderProgram);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		// clear color and depth buffer
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		glUseProgram(shaderProgram);
+
+		// draw square
+		glBindVertexArray(VAO_Square);
+		glDrawArrays(GL_QUADS, 3, 7); //mode,first,count
+
+		// draw triangles
+		//glBindVertexArray(VAO_Triangle);
+		//glDrawArrays(GL_TRIANGLES, 0, 3); //mode,first,count
+
+		// update other events like input handling
+		glfwPollEvents();
+		// put the stuff we’ve been drawing onto the display
+		glfwSwapBuffers(window);
+	}
+	glfwDestroyWindow(window);
+	glfwTerminate();
+	exit(EXIT_SUCCESS);
+}
+
+void CheckShaders(const GLuint &shaderProgram)
+{
 	GLint status;
 	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &status);
 	if (status == GL_FALSE)
@@ -154,22 +160,16 @@ int main(void)
 		fprintf(stderr, "Linker failure : %s\n", strInfoLog);
 		delete[] strInfoLog;
 	}
+}
 
-
-	while (!glfwWindowShouldClose(window))
-	{
-		// clear color and depth buffer
-		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-		glUseProgram(shaderProgram);
-		glBindVertexArray(VAO);
-		// draw triangles
-		glDrawArrays(GL_TRIANGLES, 0, 3); //mode,first,count
-		// update other events like input handling
-		glfwPollEvents();
-		// put the stuff we’ve been drawing onto the display
-		glfwSwapBuffers(window);
-	}
-	glfwDestroyWindow(window);
-	glfwTerminate();
-	exit(EXIT_SUCCESS);
+void PrintVersions()
+{
+	printf("OpenGL Version: %s\n", glGetString(GL_VERSION));
+	printf("Using GLEW %s\n", glewGetString(GLEW_VERSION));
+	printf("Vendor %s\n", glGetString(GL_VENDOR));
+	printf("Renderer %s\n", glGetString(GL_RENDERER));
+	printf("GLSL %s\n", glGetString(GL_SHADING_LANGUAGE_VERSION));
+	int major, minor, revision;
+	glfwGetVersion(&major, &minor, &revision);
+	printf("Using GLFW %i.%i.%i\n", major, minor, revision);
 }
---
+++ ZPG/Source.h
@@ -0,0 +1,5 @@
+#pragma once
+
+void PrintVersions();
+
+void CheckShaders(const GLuint &shaderProgram);
