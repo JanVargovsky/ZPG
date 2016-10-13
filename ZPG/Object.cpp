e2347bbd44efa936c031acb1b09bb31759aa7b18
Jan Vargovský <jan.vargovsky@gmail.com>
2016-10-13 19:03:47 +0000

    [ZPG] Removed object legacy code. Imported from Changeset 48

--- ZPG/Object.cpp
+++ ZPG/Object.cpp
@@ -25,63 +25,25 @@ Object::Object(std::shared_ptr<Program> program, function<void(Object &)> update
 		0.0f,  1.0f, 0.0f,		0.0f, 0.0f, 1.0f,	0.0f,  1.0f, 0.0f,	// top
 	};
 
-	// Vertex Buffer Objects
 	vbo->BindData(sizeof(vertices), vertices);
-
-	//glGenBuffers(1, &VBO);
-	// Element Buffer Objects
-	//GLuint EBO;
-	//glGenBuffers(1, &EBO);
-	// Vertex Array Object
-	//glGenVertexArrays(1, &VAO);
-
-	// Bind the VAO, and then set which VBO it uses and how it is used (attribute pointer)
-	//glBindVertexArray(VAO);
 	vao->Bind();
 	{
 		vbo->Bind();
-		//glBindBuffer(GL_ARRAY_BUFFER, VBO);
-		//glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-		/*glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);*/
-		// 0 stands for layout=0
-		// Normalized data = data that are not between 0 and 1 will be mapped to those values
-		// stride = how big is that
 
 		// Position
-		//glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 9 * sizeof(GLfloat), (GLvoid*)(0 * sizeof(GLfloat)));
-		//glEnableVertexAttribArray(0);
 		vao->SetAttribute(0, 3, AttributeType::Float, GL_FALSE, 9 * sizeof(GLfloat), (GLvoid*)(0 * sizeof(GLfloat)));
-
 		// Color
-		//glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 9 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
-		//glEnableVertexAttribArray(1);
 		vao->SetAttribute(1, 3, AttributeType::Float, GL_FALSE, 9 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
-
 		// Normal
-		//glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, 9 * sizeof(GLfloat), (GLvoid*)(6 * sizeof(GLfloat)));
-		//glEnableVertexAttribArray(2);
 		vao->SetAttribute(2, 3, AttributeType::Float, GL_FALSE, 9 * sizeof(GLfloat), (GLvoid*)(6 * sizeof(GLfloat)));
 
-
-		// Unbind VBO
-		glBindBuffer(GL_ARRAY_BUFFER, 0);
 		vbo->Unbind();
 	}
-	// Unbind VAO
-	//glBindVertexArray(0);
 	vao->Unbind();
 }
 
 Object::~Object()
 {
-	//if (vao != nullptr)
-	//	delete vao;
-	//if (vbo != nullptr)
-	//	delete vbo;
-	//if (ebo != nullptr)
-	//	delete ebo;
 }
 
 void Object::Draw()
@@ -93,7 +55,7 @@ void Object::Draw()
 		update(*this);
 
 	program->Use();
-	//glBindVertexArray(VAO);
+
 	vao->Bind();
 	mat4 modelMatrix = GetTransform().Get();
 	program->Send("model", modelMatrix);
@@ -102,7 +64,6 @@ void Object::Draw()
 
 	glDrawArrays(GL_TRIANGLES, 0, 3);
 
-	//glBindVertexArray(0);
 	vao->Unbind();
 	program->Unuse();
 }
