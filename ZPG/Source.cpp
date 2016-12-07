0bfd8a5d56719097c90b50a6deee5602c49acec3
Jan Vargovský <jan.vargovsky@gmail.com>
2016-12-07 09:32:03 +0000

    [ZPG] Test code removed. Imported from Changeset 131

--- ZPG/Source.cpp
+++ ZPG/Source.cpp
@@ -3,29 +3,8 @@
 
 #include <time.h>
 
-#include <vector>
-#include <iostream>
-#include "BezierCurve.h"
-#include "glm/vec2.hpp"
-
-using namespace glm;
-using namespace std;
-
-void test()
-{
-	vector<vec2> points = { vec2(-3,-3),vec2(-1.5, 10), vec2(-1.5,-10), vec2(3,3) };
-	BezierCurve curve(points);
-	for (double i = 0; i <= 1.; i += 0.1)
-	{
-		auto p = curve.GetPoint(i);
-		cout << p.x << " " << p.y << endl;
-	}
-}
-
 int main()
 {
-	test();
-
 	srand(time(0));
 
 	DependencyResolver::GetInstance().Initialize();
